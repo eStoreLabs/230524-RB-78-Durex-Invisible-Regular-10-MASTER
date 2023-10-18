17b9e90ad343955e164b176843b00f81ebf6d92e
Dominika Wojewska <130984276+Domi-eStore@users.noreply.github.com>
2023-10-18 09:17:07 +0200

    correct fix animation

--- assets/js/open.js
+++ assets/js/open.js
@@ -5,7 +5,7 @@ const windowHeight = window.innerHeight;
 function handleScroll() {
   const scrollPosition = window.scrollY;
   const maxMarginBottom = -100;
-  const minMarginBottom = -35;
+  const minMarginBottom = 35;
   const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
   const scrollPercentage = scrollPosition / maxScroll;
   const margin = ((1 - scrollPercentage) * (maxMarginBottom - minMarginBottom)) + minMarginBottom;
